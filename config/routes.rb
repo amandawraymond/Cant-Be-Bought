4e0318f3ccf8f62a69a9bd1806b2b7fa82003bda
eliza-irene <elizabeth.korthof@gmail.com>
2014-12-18 15:28:04 -0500

    fixed nav arrow positioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :statistics
-
   resources :quotes
-
   resources :maps
 
   # The priority is based upon order of creation: first created -> highest priority.
